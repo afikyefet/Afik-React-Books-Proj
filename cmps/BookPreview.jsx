9a39c6265db689f07de36579dbfdbdda6d9bac61
afikyefet <afik.yefet@gmail.com>
2024-11-27 12:52:17 +0200

    redesign the book cards

--- cmps/BookPreview.jsx
+++ cmps/BookPreview.jsx
@@ -5,26 +5,31 @@ export function BookPreview({ book, onRemoveBook }) {
 
 	return (
 		<div className="book-preview">
-			<img src={book.thumbnail} alt="book cover" />
-			<h1 className="book-title">{book.title}</h1>
+			<div className="book-img">
+				<img src={book.thumbnail} alt="book cover" />
+			</div>
+			<div className="book-title">
+				<h1>{book.title}</h1>
+			</div>
 			<h5 className="book-authors">
 				{book.authors}, {book.publishedDate}
 			</h5>
 			<h5 className="book-categories">{book.categories}</h5>
 			{/* <h5>{book.pageCount}</h5> */}
-			<h4>
+			<h4 className="book-price">
 				{book.listPrice.amount} {book.listPrice.currencyCode}
+				{book.listPrice.isOnSale ? " On Sale!" : ""}
 			</h4>
-			<h5> {book.listPrice.isOnSale ? "On Sale!" : ""}</h5>
+			{/* <h5> {book.listPrice.isOnSale ? "On Sale!" : ""}</h5> */}
 			<div className="btn-container">
 				<button className="del-btn" onClick={() => onRemoveBook(book.id)}>
 					delete
 				</button>
-				<button className="edit-btn">
-					<Link to={`/book/edit/${book.id}`}>Edit</Link>
-				</button>
 				<button className="details-btn">
 					<Link to={`/book/${book.id}`}>Details</Link>
+				</button>
+				<button className="edit-btn">
+					<Link to={`/book/edit/${book.id}`}>Edit</Link>
 				</button>
 			</div>
 		</div>
